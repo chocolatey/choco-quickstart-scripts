585e43da3d71e3f6159301fedeebe1bd7bdb6daa
Adil Leghari <adilio@gmail.com>
2021-08-04 15:07:01 -0700

    Fix IIS install order in CCM script

--- scripts/Start-C4bCcmSetup2.ps1
+++ scripts/Start-C4bCcmSetup2.ps1
@@ -17,14 +17,8 @@ $DefaultEap = $ErrorActionPreference
 $ErrorActionPreference = 'Stop'
 Start-Transcript -Path "$env:SystemDrive\choco-setup\logs\Start-C4bCcmSetup2-$(Get-Date -Format 'yyyyMMdd-HHmmss').txt"
 
-# DB Setup
 $PkgSrc = "$env:SystemDrive\choco-setup\packages"
-$Ccr = "'https://community.chocolatey.org/api/v2/'"
 
-choco install aspnetcore-runtimepackagestore --version 2.2.7 --source $Ccr --no-progress -y
-choco install dotnetcore-windowshosting --version 2.2.7 --source $Ccr --no-progress -y
-choco pin add --name="'aspnetcore-runtimepackagestore'" --version="'2.2.7'" --reason="'Required for CCM website'"
-choco pin add --name="'dotnetcore-windowshosting'" --version="'2.2.7'" --reason="'Required for CCM website'"
 choco install chocolatey-management-web -y --source $PkgSrc --package-parameters-sensitive="'/ConnectionString:Server=Localhost\SQLEXPRESS;Database=ChocolateyManagement;User ID=ChocoUser;Password=$DatabaseUserPw;'"
 
 Write-Host "CCM Setup has now completed" -ForegroundColor Green
