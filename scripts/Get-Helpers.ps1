76dfa2bde9793088c79f350acdbe8e4c88cd40a0
James Ruskin <james@chocolatey.io>
2024-02-02 16:40:23 +0000

    (#234) Fixes NuGet v3 Localhost Cache Issue
    Nexus Repository stores the first FQDN it hears about for all future requests. As we initially upload packages via localhost, this is less than excellent and breaks future FQDN-based requests.
    
    This commit adds:
    - A function to remove existing v3 cache folder(s)
    - A call to that function after the Nexus setup script has pushed all of the packages

--- scripts/Get-Helpers.ps1
+++ scripts/Get-Helpers.ps1
@@ -133,7 +133,7 @@ function Connect-NexusServer {
                 Uri         = $url
             }
 
-            $result = Invoke-RestMethod @params -ErrorAction Stop
+            $null = Invoke-RestMethod @params -ErrorAction Stop
             Write-Host "Connected to $Hostname" -ForegroundColor Green
         }
 
@@ -172,16 +172,16 @@ function Invoke-Nexus {
 
         [Parameter(Mandatory)]
         [String]
-        $Method
+        $Method,
 
-
+        [hashtable]
+        $AdditionalHeaders = @{}
     )
     process {
-
         $UriBase = "$($protocol)://$($Hostname):$($port)"
         $Uri = $UriBase + $UriSlug
         $Params = @{
-            Headers     = $header
+            Headers     = $header + $AdditionalHeaders
             ContentType = $ContentType
             Uri         = $Uri
             Method      = $Method
@@ -190,7 +190,7 @@ function Invoke-Nexus {
         if ($Body) {
             $Params.Add('Body', $($Body | ConvertTo-Json -Depth 3))
         } 
-        
+
         if ($BodyAsArray) {
             $Params.Add('Body', $($BodyAsArray | ConvertTo-Json -Depth 3))
         }
@@ -422,6 +422,60 @@ function Remove-NexusRepository {
             catch {
                 $_.exception.message
             }
+        }
+    }
+}
+
+function Remove-NexusRepositoryFolder {
+    <#
+    .SYNOPSIS
+    Removes a given folder from a repository from the Nexus instance
+
+    .PARAMETER RepositoryName
+    The repository to remove from
+
+    .PARAMETER Name
+    The name of the folder to remove
+
+    .EXAMPLE
+    Remove-NexusRepositoryFolder -RepositoryName MyNuGetRepo -Name 'v3'
+    # Removes the v3 folder in the MyNuGetRepo repository
+    #>
+    [CmdletBinding()]
+    param(
+        [Parameter(Mandatory)]
+        [string]$RepositoryName,
+
+        [Parameter(Mandatory)]
+        [string]$Name
+    )
+    end {
+        if (-not $header) {
+            throw "Not connected to Nexus server! Run Connect-NexusServer first."
+        }
+
+        $ApiParameters = @{
+            UriSlug = "/service/extdirect"
+            Method  = "POST"
+            Body    = @{
+                action = "coreui_Component"
+                method =  "deleteFolder"
+                data   = @(
+                    $Name,
+                    $RepositoryName
+                )
+                type   = "rpc"
+                tid    = Get-Random -Minimum 1 -Maximum 100
+            }
+            AdditionalHeaders = @{
+                "X-Nexus-UI" = "true"
+            }
+        }
+
+        $Result = Invoke-Nexus @ApiParameters
+
+        if (-not $Result.result.success) {
+            throw "Failed to delete folder: $($Result.result.message)"
         }
     }
 }
